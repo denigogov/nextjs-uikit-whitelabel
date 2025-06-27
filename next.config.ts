60b95a03519c16fbf966f687bb8c218504274de1
Dejan Gogov <denigogov@gmail.com>
2025-06-27 21:43:43 +0200

    scss config file update

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,7 @@ const path = require("path");
 const nextConfig: NextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "src")],
-    prependData: `
-        @import "@/global/scss/_app.scss";
-      `,
-  },
-
-  compiler: {
-    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
-    styledComponents: {},
+    additionalData: `@use "@/global/scss/_app.scss" as *;`,
   },
 };
 
