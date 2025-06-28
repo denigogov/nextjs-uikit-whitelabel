b343dfebeb5ab17a697f8440ebd59b059472cb51
Dejan Gogov <denigogov@gmail.com>
2025-06-28 22:44:31 +0200

    functional multilanguage feature

---
+++ src/i18n/routing.ts
@@ -0,0 +1,16 @@
+import { defineRouting } from "next-intl/routing";
+
+export const routing = defineRouting({
+  localeDetection: true,
+
+  // A list of all locales that are supported
+  locales: ["en", "de"],
+
+  // Used when no locale matches
+  defaultLocale: "en",
+  pathnames: {
+    "/about": {
+      de: "/über-uns",
+    },
+  },
+});
